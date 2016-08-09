48d0e6199e849ad4032555a739f54cc6cd07215c
Deimer Ramos Cuello <dramos.cuello@gmail.com>
2016-08-09 02:23:06 -0500

    volcado de txt a bd

--- ValidacionesSalud/Default.aspx.vb
+++ ValidacionesSalud/Default.aspx.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Data
 Imports MySql.Data.MySqlClient
+Imports System.Text
+Imports System.IO
 
 Public Class _Default
     Inherits Page
@@ -11,11 +13,23 @@ Public Class _Default
     End Sub
 
     Protected Sub Button3_Click1(sender As Object, e As EventArgs) Handles Button3.Click
-        con.Open()
-        Dim cmd As MySqlCommand = con.CreateCommand()
-        cmd.CommandType = CommandType.Text
-        cmd.CommandText = "truncate prueba"
-        cmd.ExecuteNonQuery()
-        con.Close()
+        'con.Open()
+        'Dim cmd As MySqlCommand = con.CreateCommand()
+        'cmd.CommandType = CommandType.Text
+        'cmd.CommandText = "truncate prueba"
+        'cmd.ExecuteNonQuery()
+        'con.Close()
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim path As String = "C:\\Users\\Win10\\Dropbox\\Guardados\\" + FileUpload1.PostedFile.FileName
+        If Not String.IsNullOrEmpty(path) Then
+            con.Open()
+            Dim cmd As MySqlCommand = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "LOAD DATA LOCAL INFILE " + "'" + path + "'" + " INTO TABLE datosusutext FIELDS TERMINATED BY ',' ENCLOSED BY " + "'" + "\" + Chr(34) + "'" + " ESCAPED BY " + "'" + "\" + "\" + "'"
+            cmd.ExecuteNonQuery()
+            con.Close()
+        End If
     End Sub
 End Class
